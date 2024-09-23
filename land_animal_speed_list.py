f4d4f5a88f6c361c45ac4c317e1ab5c52bb0cd9b
carol0221 <CarolEngo21@gmail.com>
2024-09-23 15:55:11 +0800

    land_animal_list.py

---
+++ land_animal_speed_list.py
@@ -0,0 +1,15 @@
+land_animal = ["Mexican pronghorn-88km/h", "Springbok-88km/h", "Thomson's gazelle-96km/h", "Blacbuck-80km/h", "American quarter horse- 80km/h", "Wildebeest-80km/h", "Bronw hare-70km/h", "Ostrich-70km/h", "Afghan hound-70km/h", "Greyhound-70km/h", "African wild dog-70km/h", "Lion-60km/h"]
+
+print(land_animal)
+
+print(land_animal[6])
+
+land_animal[8] = "Cheetah-120km/h"
+print(land_animal)
+
+del land_animal[9]
+print(land_animal)
+
+print(land_animal[2:7])
+
+print(land_animal[-1])
