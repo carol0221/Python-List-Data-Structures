0b1dae154f04373eec256f73251dc3eac8cb944d
carol0221 <CarolEngo21@gmail.com>
2024-09-23 13:29:11 +0800

    animal_species_list.py

---
+++ animal_species_list.py
@@ -0,0 +1,13 @@
+animal_species =["Blue whale", "Chimpanzee", "Tiger", "Wolf", "Cheetah", "Saola", "Polar bear"]
+
+print(animal_species)
+
+print(animal_species[1])
+
+animal_species[4] = "Gorilla"
+print(animal_species)
+
+del animal_species[5]
+print(animal_species)
+
+print(animal_species[-1])
