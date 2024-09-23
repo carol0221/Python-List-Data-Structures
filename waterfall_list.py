ed5be848a5ac1764525c15f76de954601efe97db
carol0221 <CarolEngo21@gmail.com>
2024-09-23 16:24:05 +0800

    waterfall_list.py

---
+++ waterfall_list.py
@@ -0,0 +1,13 @@
+waterfalls = ["Victoria Falls", "Angel Falls", "Palouse Falls", "Iguazu Falls", "Gullfoss Falls", "Yosemite Falls", "Sutherland Falls", "Jog Falls", "Tugela Falls", "Rhine FalLs"]
+
+print(waterfalls)
+
+print(waterfalls[7])
+
+waterfalls[4] = "Niagra Falls"
+print(waterfalls)
+
+del waterfalls[8]
+print(waterfalls)
+
+print(waterfalls[-1])
