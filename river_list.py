909f2ffcd0363221b9bb33576fafd05841804643
carol0221 <CarolEngo21@gmail.com>
2024-09-23 16:09:50 +0800

    river_list.py

---
+++ river_list.py
@@ -0,0 +1,13 @@
+river = ["Indus River", "Mississippi River", "Amazon River", "Ganges", "Godovari River", "Mekong River", "Volga River", "Yellow River", "Salween River", "Rio Grande"]
+
+print(river)
+
+print(river[5])
+
+river[7] = "Nile River"
+print(river)
+
+del river[8]
+print(river)
+
+print(river[-1])
