78dc1868f6c4daf45b2f5b57713cbc2b88aad472
carol0221 <CarolEngo21@gmail.com>
2024-09-23 13:55:29 +0800

    actor_list.py

---
+++ actor_list.py
@@ -0,0 +1,13 @@
+actors = ["Hugh Jackman", "Tom Cruise", "Liam Neeson", "Robert De Niro", "Will Smith", "Brad Pitt", "Matt Damon", "George Clooney", "Tom Hunks", "Bradley Cooper"]
+
+print(actors)
+
+print(actors[6])
+
+actors[2] = "Leonardo DiCaprio"
+print(actors)
+
+del actors[7]
+print(actors)
+
+print(actors[-1])
