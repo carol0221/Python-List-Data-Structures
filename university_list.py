889ede75541fb1995e7c9179d3ded6152a5d090c
carol0221 <CarolEngo21@gmail.com>
2024-09-23 14:54:09 +0800

    university_list.py

---
+++ university_list.py
@@ -0,0 +1,13 @@
+universities = ["Stanford University", "University of Oxford", "Yale University", "Ateneo de Manila University", "Far Eastern University", "De La Salle University Manila", "Peking University", "Princeton University", "University of Santo Thomas", "University of Cambridge"]
+
+print(universities)
+
+print(universities[5])
+
+universities[3] = "Harvard University"
+print(universities)
+
+del universities[8]
+print(universities)
+
+print(universities[-1])
