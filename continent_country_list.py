446a534d3ffddd188b49bc98a62c7532342fc6e7
carol0221 <CarolEngo21@gmail.com>
2024-09-23 15:22:11 +0800

    continent_country_list.py

---
+++ continent_country_list.py
@@ -0,0 +1,15 @@
+continent_country = ["Asia-Korea", "North America-Canada", "Asia-Japan", "South America-Argentina", "Africa-Cameroon", "Asia-India", "North America-Cuba", "South America-Peru", "Europe-Austria", "Asia-Thailand", "Europe-Albania", "Africa-Sudan", "Europe- Belgium", "Asia-Israel", "North America-Costa Rica"]
+
+print(continent_country)
+
+print(continent_country[8])
+
+continent_country[9] = "Australia"
+print(continent_country)
+
+del continent_country[11]
+print(continent_country)
+
+print(continent_country[3:8])
+
+print(continent_country[-1])
