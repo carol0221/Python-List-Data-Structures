336ef4950d0d445dfce63bf61342631b27ffa444
carol0221 <CarolEngo21@gmail.com>
2024-09-23 13:41:15 +0800

    furniture_list.py

---
+++ furniture_list.py
@@ -0,0 +1,13 @@
+furnitures = ["Table", "Chair", "Bed", "Cabinet", "Dresser", "Mattress", "Nigthstand", "Vanity", "Recliner", "Bookshelf", "Barstool", "Wardrobe"]
+
+print(furnitures)
+
+print(furnitures[7])
+
+furnitures[4] = "Sofa"
+print(furnitures)
+
+del furnitures[9]
+print(furnitures)
+
+print(furnitures[-1])
