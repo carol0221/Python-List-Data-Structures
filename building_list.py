1329050af55467bc7fcb3d084e3777ca06c74552
carol0221 <CarolEngo21@gmail.com>
2024-09-23 13:51:32 +0800

    building_list.py

---
+++ building_list.py
@@ -0,0 +1,13 @@
+buildings = ["Burj Khalifa", "Colosseum", "Sydney Opera House", "Tower of Pisa", "Lotus Temple", "The Shard", "Pantheon", "The White House"]
+
+print(buildings)
+
+print(buildings[4])
+
+buildings[1] = "Eiffel Tower"
+print(buildings)
+
+del buildings[5]
+print(buildings)
+
+print(buildings[-1])
