cfc5cfb6bff853b8562807a30984042e1d7ceb3c
carol0221 <CarolEngo21@gmail.com>
2024-09-23 13:01:50 +0800

    students_list.py

---
+++ students_list.py
@@ -0,0 +1,16 @@
+student_names = ["Lay", "Welius", "Haze", "Anniza", "Jeo", "Beckett", "Faith", "Evren", "Fairen", "Blade", "Mace", "Blake", "Phaxton", "Ace", "Ruth", "Blair", "Blaze","Vanessa", "Kylie", "Jay"]
+
+print(student_names)
+
+
+print(student_names[14])
+
+student_names[11] = "John"
+print(student_names)
+
+del student_names[9]
+print(student_names)
+
+print(student_names[1:5])
+
+print(student_names[-1])
