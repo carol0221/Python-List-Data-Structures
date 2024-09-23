d88f99715c022dd9e4d76969f0a28e2bc8f65180
carol0221 <CarolEngo21@gmail.com>
2024-09-23 14:08:10 +0800

    ocean_list.py

---
+++ ocean_list.py
@@ -0,0 +1,13 @@
+ocean = ["Arctic Ocean", "North Atlantic Ocean", "Southern Ocean", "Indian Ocean", "South Atlantic Ocean"]
+
+print(ocean)
+
+print(ocean[2])
+
+ocean[1] = "Pacific Ocean"
+print(ocean)
+
+del ocean[4]
+print(ocean)
+
+print(ocean[-1])
