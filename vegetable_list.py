58971e5abcc83686161a142c51c14d949d621342
carol0221 <CarolEngo21@gmail.com>
2024-09-23 13:10:50 +0800

    vegetable_list.py

---
+++ vegetable_list.py
@@ -0,0 +1,13 @@
+vegetables = ["Broccoli", "Cabbage", "Tomato", "Cauliflower", "Carrot", "Squash", "Eggplant"]
+
+print(vegetables)
+
+print(vegetables[4])
+
+vegetables[2] = "Spinach"
+print(vegetables)
+
+del vegetables[5]
+print(vegetables)
+
+print(vegetables[-1])
