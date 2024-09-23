111b35a192d30dd8ed4ea8fe2911d429dfd13789
carol0221 <CarolEngo21@gmail.com>
2024-09-23 14:39:17 +0800

    drink_list.py

---
+++ drink_list.py
@@ -0,0 +1,13 @@
+drinks = ["Tea", "Milk", "Water", "Milkshake", "Juice", "Soda", "Iced Tea", "Lemonade"]
+
+print(drinks)
+
+print(drinks[3])
+
+drinks[2] = "Coffee"
+print(drinks)
+
+del drinks[6]
+print(drinks)
+
+print(drinks[-1])
