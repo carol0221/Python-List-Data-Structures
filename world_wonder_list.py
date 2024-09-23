ceee3d2f31a3c44959b39dda6f30d85386822366
carol0221 <CarolEngo21@gmail.com>
2024-09-23 16:40:26 +0800

    world_wonder_list.py

---
+++ world_wonder_list.py
@@ -0,0 +1,13 @@
+world_wonders = ["Colosseum", "Taj Mahal", "Christ the Redemeer", "Petra", "The Temple of Artemis", "Chichen Itza", "Machu Picchu"]
+
+print(world_wonders)
+
+print(world_wonders[3])
+
+world_wonders[1] = "Great Wall of China"
+print(world_wonders)
+
+del world_wonders[4]
+print(world_wonders)
+
+print(world_wonders[-1])
