1ff6457e7a7a8c2adf3647df38b12697319aa0f7
carol0221 <CarolEngo21@gmail.com>
2024-09-23 13:25:57 +0800

    company_list.py

---
+++ company_list.py
@@ -0,0 +1,13 @@
+company = ["Microsoft", "Amazon", "Manila Electric Co", "Alphabet", "Meta", "Saudi Aramco", "Shell Philippines Corporation", "Berkshire Hathway", "Infosys", "Bank of the Philippine Islands"]
+
+print(company)
+
+print(company[6])
+
+company[4] = "Apple"
+print(company)
+
+del company[7]
+print(company)
+
+print(company[-1])
