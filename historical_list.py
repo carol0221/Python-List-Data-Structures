a0b63d5b1e3ab6749973ccf2fe9a6215e0649b51
carol0221 <CarolEngo21@gmail.com>
2024-09-23 13:30:50 +0800

    historical_list.py

---
+++ historical_list.py
@@ -0,0 +1,13 @@
+historical_figures = ["Abraham Lincoln", "George Washington", "Franklin delano Roosevelt", "Mahatma Gandhi", "Adolf Hitler", "Elizabeth I", "Charles Darwin", "Mother theresa", "John F Kenedy", "Malala Yousafzai"]
+
+print(historical_figures)
+
+print(historical_figures[7])
+
+historical_figures[3] = "Nelson Mandela"
+print(historical_figures)
+
+del historical_figures[7]
+print(historical_figures)
+
+print(historical_figures[-1])
