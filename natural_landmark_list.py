763cd776fec0b52819aeacd694000b51a9e72bc4
carol0221 <CarolEngo21@gmail.com>
2024-09-23 15:41:23 +0800

    natural_landmark_list.py

---
+++ natural_landmark_list.py
@@ -0,0 +1,13 @@
+natural_landmark = ["Victoria Falls", "Uluru", "Uyuni Salt Flat", "Verdon gorge", "Pangong Tso", "Mount Bromo", "Pulpit Rock", "Batad Rice Terraces", "Angel Falls", "Dead Sea"]
+
+print(natural_landmark)
+
+print(natural_landmark[7])
+
+natural_landmark[4] = "Grand Canyon"
+print(natural_landmark)
+
+del natural_landmark[8]
+print(natural_landmark)
+
+print(natural_landmark[-1])
