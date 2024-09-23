88e091668980cf56e9c03dbafafc512919fe66d6
carol0221 <CarolEngo21@gmail.com>
2024-09-23 16:47:54 +0800

    space_mission_list.py

---
+++ space_mission_list.py
@@ -0,0 +1,13 @@
+space_missions = ["Lunar Orbiter 1", "Voyager", "Mars Express", "Gemini", "Pioneer 10", "Marine program", "Parker Solar Probe", "Project Mercury", "Skylab", "Space Shuttle"]
+
+print(space_missions)
+
+print(space_missions[6])
+
+space_missions[3] = "Apollo 11"
+print(space_missions)
+
+del space_missions[5]
+print(space_missions)
+
+print(space_missions[-1])
