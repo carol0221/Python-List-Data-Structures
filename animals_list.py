a7fc6fc983581ad187e03e70bcbca78b396f3b17
carol0221 <CarolEngo21@gmail.com>
2024-09-23 13:05:58 +0800

    animals_list.py

---
+++ animals_list.py
@@ -0,0 +1,13 @@
+animals = ["Dog", "Cat", "Hamster", "Bear", "Alligator", "Zebra", "Fox", "Panda", "Monkey", "Lion"]
+
+print(animals)
+
+print(animals[2])
+
+animals[5] = "Elephant"
+print(animals)
+
+del animals[7]
+print(animals)
+
+print(animals[-1])
