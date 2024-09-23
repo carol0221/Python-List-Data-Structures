7301a6ecfdefd0c5b284cc389194098237c4d2ae
carol0221 <CarolEngo21@gmail.com>
2024-09-23 13:09:00 +0800

    fruit_list.py

---
+++ fruit_list.py
@@ -0,0 +1,16 @@
+fruits = ["Kiwis", "Peaches", "Grapes", "Watermelons", "Berries", "Cherries", "Bananas", "Avocados", "Apples", "Oranges", "Pears", "Plums"]
+
+print(fruits)
+
+print(fruits[8])
+
+fruits[1] = "Mangoes"
+print(fruits)
+
+del fruits[9]
+print(fruits)
+
+
+print(fruits[3:7])
+
+print(fruits[-1])
