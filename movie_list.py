c4132b02d3a357b4be2f56f7d2810eb02278ff1e
carol0221 <CarolEngo21@gmail.com>
2024-09-23 13:12:17 +0800

    movie_list.py

---
+++ movie_list.py
@@ -0,0 +1,17 @@
+movies = ["Inside out", "Conjuring", "Candle in the tomb", "Elemental", "The moon", "Fall", "Venom", "Skyfall", "Avatar", "Avengers", "Culpa Mia", "Dune", "Arrival", "Gravity", "Luck", "Inkheart", "A quite place", "Percy Jackson", "Charlie and the chocolate factory", "Eragon"]
+
+print(movies)
+
+print(movies[11])
+
+movies[14] = "Incception"
+print(movies)
+
+del movies[17]
+print(movies)
+
+print(movies[7:13])
+
+print(movies[-1])
+
+
