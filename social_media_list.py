c98bff1dda8afe1ecd91d0868d54c8fb57630341
carol0221 <CarolEngo21@gmail.com>
2024-09-23 13:43:37 +0800

    social_media_list.py

---
+++ social_media_list.py
@@ -0,0 +1,13 @@
+social_media = ["Facebook", "Tiktok", "Telegram", "Twitter", "Snapchat", "Weverse", "Pinterest", "Youtube", "Weibo", "WeChat"]
+
+print(social_media)
+
+print(social_media[4])
+
+social_media[2] = "Instagram"
+print(social_media)
+
+del social_media[7]
+print(social_media)
+
+print(social_media[-1])
