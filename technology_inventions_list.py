6fe14432065673dfb6b1bb606c2dcdf126731c1a
carol0221 <CarolEngo21@gmail.com>
2024-09-23 14:01:28 +0800

    technology_inventions_list.py

---
+++ technology_inventions_list.py
@@ -0,0 +1,15 @@
+technology_inventions = ["Telephone", "Computer", "Electricity", "Robotics", "Internet", "Google Glass", "Television", "Printing press", "3D printing", "Electroplating" "Global Positioning System", "Motor", "Aircraft", "Digital video recorder", "Camera"]
+
+print(technology_inventions)
+
+print(technology_inventions[10])
+
+technology_inventions[7] = "Smartphone"
+print(technology_inventions)
+
+del technology_inventions[13]
+print(technology_inventions)
+
+print(technology_inventions[4:10])
+
+print(technology_inventions[-1])
