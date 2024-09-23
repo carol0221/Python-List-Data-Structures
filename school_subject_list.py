25c6d4ec6e67dc52a6343e5220ea0bcd2640e776
carol0221 <CarolEngo21@gmail.com>
2024-09-23 13:57:35 +0800

    schhol_subject_list.py

---
+++ school_subject_list.py
@@ -0,0 +1,15 @@
+subjects = ["English", "Filipino", "Science", "Social Studies", "History", "Pathfit", "Philosophy", "Personal Development", "General Biology", "Calculus", "Social Science", "Applied Economics"]
+
+print(subjects)
+
+print(subjects[5])
+
+subjects[7] = "Mathematics"
+print(subjects)
+
+del subjects[3]
+print(subjects)
+
+print(subjects[1:5])
+
+print(subjects[-1])
