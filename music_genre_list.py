3c8f40af2bba47b14fa3c78ac377d489d897f2f5
carol0221 <CarolEngo21@gmail.com>
2024-09-23 13:49:37 +0800

    music_genre_list.py

---
+++ music_genre_list.py
@@ -0,0 +1,13 @@
+music_genre = ["Rock", "Pop", "Blues", "Rhythm and Blues", "Classical", "Hip hop music", "Funk", "Folk music", "Soul music", "Disco", "Synth-pop", "country", "Rap", "Progressive Rock", "Latin music"]
+
+print(music_genre)
+
+print(music_genre[9])
+
+music_genre[4] = "Jazz"
+print(music_genre)
+
+del music_genre[6]
+print(music_genre)
+
+print(music_genre[-1])
