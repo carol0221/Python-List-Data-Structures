12152c78ea0f8ea3deb247e242d100f2b0c39e3c
carol0221 <CarolEngo21@gmail.com>
2024-09-23 13:34:25 +0800

    holiday_list.py

---
+++ holiday_list.py
@@ -0,0 +1,15 @@
+holidays = ["New Year's Day", "Maunday Thursday", "Good Friday", "Independence Day", "Labour Day", "Ninoy Aquino Day", "Bonificio Day", "Rizal Day", "Eid al Fitir", "National Heroes Day", "Feast of Immaculate Conception", "Day of Valor"]
+
+print(holidays)
+
+print(holidays[8])
+
+holidays[2] = "Christmas"
+print(holidays)
+
+del holidays[10]
+print(holidays)
+
+print(holidays[1:7])
+
+print(holidays[-1])
