f209dbdcd88960896c6fa37b7c65515b959da31b
carol0221 <CarolEngo21@gmail.com>
2024-09-23 13:32:25 +0800

    invention_list.py

---
+++ invention_list.py
@@ -0,0 +1,13 @@
+inventions = ["Printing press", "Robotics", "Computer", "Smartphones", "Internet", "Televison", "Electricity", "Cars", "Cassette tape"]
+
+print(inventions)
+
+print(inventions[5])
+
+inventions[1] = "Lightbulb"
+print(inventions)
+
+del inventions[4]
+print(inventions)
+
+print(inventions[-1])
