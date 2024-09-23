21ffaeba226525a32c1836660dedc3ffca09e20c
carol0221 <CarolEngo21@gmail.com>
2024-09-23 13:13:38 +0800

    game_list.py

---
+++ game_list.py
@@ -0,0 +1,16 @@
+games = ["Mobile Legends", "Temple run", "Narrow one", "Prop hunt", "Tunnel rush", "Monkey mart", "Subway surfers", "Call of Duty", "Street fighter", "Parkour race", 
+"Retro bowl", "Basketball start", "LUDUS", "Sword Master", "Combat arms"]
+
+print(games)
+
+print(games[6])
+
+games[3] = "Minecraft"
+print(games)
+
+del games[8]
+print(games)
+
+print(games[4:10])
+
+print(games[-1])
