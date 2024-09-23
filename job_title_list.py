e26522afe541e6f99a18ba724d95ec473d1d735b
carol0221 <CarolEngo21@gmail.com>
2024-09-23 14:05:40 +0800

    job_title_list.py

---
+++ job_title_list.py
@@ -0,0 +1,14 @@
+job_titles = ["Account Executive", "Sales Manager", "Engineer", "Nurse", "Teacher", "Financial Analyst", "Chemical Engineer", "Bank Teller", "Auditor", "Virtual Assistant"]
+
+print(job_titles)
+
+print(job_titles[5])
+
+job_titles[4] = "Software Engineer"
+print(job_titles)
+
+del job_titles[8]
+print(job_titles)
+
+print(job_titles[-1])
+
