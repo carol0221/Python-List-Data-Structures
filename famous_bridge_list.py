0c24411b93115ab9a0356a2f3abc739faad7defe
carol0221 <CarolEngo21@gmail.com>
2024-09-23 16:31:29 +0800

    famous_bridge_list.py

---
+++ famous_bridge_list.py
@@ -0,0 +1,13 @@
+bridge = ["Tower Bridge", "Ponte Vecchio", "Akashi-Kaikyo Bridge", "Brooklyn Bridge", "Sydney Harbour Bridge", "Charles Bridge", "Forth Bridge", "Khjoo Bridge"]
+
+print(bridge)
+
+print(bridge[2])
+
+bridge[5] = "Golden Gate Bridge"
+print(bridge)
+
+del bridge[6]
+print(bridge)
+
+print(bridge[-1])
