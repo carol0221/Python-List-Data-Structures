005e86726f7284b1e29124989c703a3adf87eab0
carol0221 <CarolEngo21@gmail.com>
2024-09-23 14:03:34 +0800

    animal_habitat_list.py

---
+++ animal_habitat_list.py
@@ -0,0 +1,13 @@
+animal_habitats = ["Desert", "GrassLand", "Forest", "Lake", "Cave", "Polar Region", "Tundra", "Ocean"]
+
+print(animal_habitats)
+
+print(animal_habitats[4])
+
+animal_habitats[2] = "Savannah"
+print(animal_habitats)
+
+del animal_habitats[5]
+print(animal_habitats)
+
+print(animal_habitats[-1])
