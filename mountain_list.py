7907aba59931f675c16246559dfafe75227d9924
carol0221 <CarolEngo21@gmail.com>
2024-09-23 13:24:09 +0800

    mountain_list.py

---
+++ mountain_list.py
@@ -0,0 +1,13 @@
+mountains = ["K2", "Kamet", "Nanda Devi", "Mount Ararat", "Manaslu", "Mount Fuji", "Mount Logan", "Anamudi"]
+
+print(mountains)
+
+print(mountains[4])
+
+mountains[2] = "Everest"
+print(mountains)
+
+del mountains[5]
+print(mountains)
+
+print(mountains[-1])
