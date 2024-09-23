b418035c8f46942a5c2630fdaf25848094e766f1
carol0221 <CarolEngo21@gmail.com>
2024-09-23 16:15:42 +0800

    volcano_list.py

---
+++ volcano_list.py
@@ -0,0 +1,13 @@
+volcanoes = ["Mount Etna", "Mount Pinatubo", "Krakatoa", "Mount Bromo", "Mauna Loa", "Mayon Volcano", "Arenal Volcano"]
+
+print(volcanoes)
+
+print(volcanoes[3])
+
+volcanoes[2] = "Mount Vesuvius"
+print(volcanoes)
+
+del volcanoes[4]
+print(volcanoes)
+
+print(volcanoes[-1])
