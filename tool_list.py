215fe622614c8ce26bc2b89f1c5889cd95ae5703
carol0221 <CarolEngo21@gmail.com>
2024-09-23 13:59:04 +0800

    tool_list.py

---
+++ tool_list.py
@@ -0,0 +1,13 @@
+tools = ["Drill", "Pliers", "Saw", "Srewdriver", "Axe", "Wrench", "Chisel", "Ruler", "Hacksaw", "Heat Gun"]
+
+print(tools)
+
+print(tools[3])
+
+tools[6] = "Hammer"
+print(tools)
+
+del tools[4]
+print(tools)
+
+print(tools[-1])
