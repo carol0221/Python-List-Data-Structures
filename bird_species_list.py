7a05df5460c43bca021648359679a4cf55460e40
carol0221 <CarolEngo21@gmail.com>
2024-09-23 14:10:13 +0800

    bird_species_list.py

---
+++ bird_species_list.py
@@ -0,0 +1,17 @@
+bird_species = ["Golden Eagle", "Common Raven", "Wild Turkey", "Wood Duck", "Whooping crane", "Sandhill crane", "Common Loon", "Clark's Nutcraker" , "Ancient Murrelet", "Black-Headed Grosbeak", "Calliope Hummingbird", "House Finch"]
+
+print(bird_species)
+
+print(bird_species[8])
+
+bird_species[5] = "Eagle"
+print(bird_species)
+
+del bird_species[7]
+print(bird_species)
+
+print(bird_species[2:7])
+
+print(bird_species[-1])
+
+
