6845449bf711068cefdde549573701344dfab3f6
carol0221 <CarolEngo21@gmail.com>
2024-09-23 13:19:07 +0800

    flower_list.py

---
+++ flower_list.py
@@ -0,0 +1,13 @@
+flowers = ["Lily", "Tulips", "Dahlia", "Daisy", "Sunflower", "Carnation", "Orchid", "Iris", "Cosmos", "Aster"]
+
+print(flowers)
+
+print(flowers[4])
+
+flowers[7] = "Rose"
+print(flowers)
+
+del flowers[1]
+print(flowers)
+
+print(flowers[-1])
