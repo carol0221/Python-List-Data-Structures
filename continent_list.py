de6fa9fd346bba02475a74026fb2aec1ac188a8c
carol0221 <CarolEngo21@gmail.com>
2024-09-23 13:39:14 +0800

    continent_list.py

---
+++ continent_list.py
@@ -0,0 +1,13 @@
+continent = ["Asia", "Oceania", "North America", "South America", "Europe", "Antarctica", "Africa"]
+
+print(continent)
+
+print(continent[3])
+
+continent[1] = "Africa"
+print(continent)
+
+del continent[5]
+print(continent)
+
+print(continent[-1])
