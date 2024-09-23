ac98b3380d3464ced7f04564d5e91c69f421c511
carol0221 <CarolEngo21@gmail.com>
2024-09-23 14:34:34 +0800

    recipe_list.py

---
+++ recipe_list.py
@@ -0,0 +1,15 @@
+recipe = ["Pancake", "Cabbage Salad", "Chicken Fried Steak", "Adobo", "Carbonara", "Banana Bread", "Paella", "Kare-Kare", "Sushi", "Mac and Cheese", "Maja Blanca", "Pork Steak", "Beef Bulalo", "Chicken Fillet", "Fish Fillet"]
+
+print(recipe)
+
+print(recipe[11])
+
+recipe[8] = "Lasagna"
+print(recipe)
+
+del recipe[10]
+print(recipe)
+
+print(recipe[4:10])
+
+print(recipe[-1])
