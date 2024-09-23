eade4a2e39d766683c36f9c2fad9982f3e028e15
carol0221 <CarolEngo21@gmail.com>
2024-09-23 13:37:46 +0800

    festival_list.py

---
+++ festival_list.py
@@ -0,0 +1,12 @@
+festivals = ["Holi Festival", "Songkran", "Dia de los Muertos", "Maskara Festival", "Venice Carnival", "Cannes Film Festival", "Glastonbury Festival", "Boryeong Mund Festival", "Inti Raymi", "Gion Matsuri", "Obon", "Montreal International Jazz Festival", "Saint Patrick's Festival", "La Tomatina"]
+
+print(festivals)
+
+print(festivals[6])
+
+festivals[10] = "Diwali"
+print(festivals)
+
+print(festivals[4:12])
+
+print(festivals[-1])
