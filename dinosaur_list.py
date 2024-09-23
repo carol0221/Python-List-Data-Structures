3baa680e7f700f85e0b90a307a74c0f1b893025d
carol0221 <CarolEngo21@gmail.com>
2024-09-23 15:33:56 +0800

    dinosaur_list.py

---
+++ dinosaur_list.py
@@ -0,0 +1,14 @@
+dinosaur = ["Spinosaurus", "Velociprator", "Brachiosaurus", "Diplodocus", "Dilophosaurus", "Brontosaurus", "Allosaurus", "Troodon"]
+
+print(dinosaur)
+
+print(dinosaur[3])
+
+dinosaur[5] = "Tyrannosaurus Rex"
+print(dinosaur)
+
+del dinosaur[6]
+print(dinosaur)
+
+print(dinosaur[-1])
+
