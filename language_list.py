fdfe1ecfca86e4c3e315ebc93d2ae78ff072481c
carol0221 <CarolEngo21@gmail.com>
2024-09-23 13:17:39 +0800

    language_list.py

---
+++ language_list.py
@@ -0,0 +1,15 @@
+programming_languages = ["C#", "Java", "SQL", "PHP", "GO", "JavaScript", "R", "Swift", "MATLAB", "Ruby", "Scala", "CSS", "Kotlin", "Objective-C"]
+
+print(programming_languages)
+
+print(programming_languages[10])
+
+programming_languages[8] = "Python"
+print(programming_languages)
+
+del programming_languages[12]
+print(programming_languages)
+
+print(programming_languages[4:12])
+
+print(programming_languages[-1])
