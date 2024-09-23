da7dd936040048bdbe136e31894b5f66655f5fe0
carol0221 <CarolEngo21@gmail.com>
2024-09-23 14:19:21 +0800

    currency_list.py

---
+++ currency_list.py
@@ -0,0 +1,13 @@
+currency = ["United State Dollar", "Indian Rupee", "Canadian Dollar", "Swiss Franc", "Philippine Peso", "Chinese Yuan", "Bruinei Dollar", "Baht", "Korean Won", "Chilean Peso"]
+
+print(currency)
+
+print(currency[4])
+
+currency[6] = "Euro"
+print(currency)
+
+del currency[8]
+print(currency)
+
+print(currency[-1])
