f9947f354a0341d60cdd8882de7d44ad5099b213
carol0221 <CarolEngo21@gmail.com>
2024-09-23 13:35:50 +0800

    dessert_list.py

---
+++ dessert.py
@@ -0,0 +1,13 @@
+dessert = ["Brownies", "Leche Flan", "Halo-Halo", "Egg Pie", "Carrot Cake", "Cookies", "Ice Cream", "Cupcakes", "Maja Blanca", "Buko Salad"]
+
+print(dessert)
+
+print(dessert[3])
+
+dessert[5] = "Chocolate Cake"
+print(dessert)
+
+del dessert[2]
+print(dessert)
+
+print(dessert[-1])
