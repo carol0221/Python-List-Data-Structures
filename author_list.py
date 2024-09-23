3359db757da4a822b92f62d77ab9a7caacec3157
carol0221 <CarolEngo21@gmail.com>
2024-09-23 13:20:46 +0800

    author_list.py

---
+++ author_list.py
@@ -0,0 +1,15 @@
+authors = ["George orwell", "James Joyce", "Lewis carroll", "France Kafka", "William Shakespeare", "Stephen King", "Charles Dickens", "Jane Austen", "Toni Morrison","Marcel Proust", "John Milton", "George Eliot"]
+
+print(authors)
+
+print(authors[5])
+
+authors[3] = "Mark Twain"
+print(authors)
+
+del authors[9]
+print(authors)
+
+print(authors[2:8])
+
+print(authors[-1])
