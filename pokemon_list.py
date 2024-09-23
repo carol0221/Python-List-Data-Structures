42badf6dd2c371c1465cdb7a44b1bcbd98b1ee42
carol0221 <CarolEngo21@gmail.com>
2024-09-23 13:27:29 +0800

    pokemon_list.py

---
+++ pokemon_list.py
@@ -0,0 +1,15 @@
+pokemon = ["Bulbasaur", "Charmander", "Charmeleon", "Squirtle", "Caterpie", "Metapod", "Weedle", "Raticat", "Pidgeot", "Rattatak", "Spearow", "Fearow", "Nagahis", "Nagaasp", "Raichu"]
+
+print(pokemon)
+
+print(pokemon[8])
+
+pokemon[11] = "Pikachu"
+print(pokemon)
+
+del pokemon[9]
+print(pokemon)
+
+print(pokemon[3:7])
+
+print(pokemon[-1])
