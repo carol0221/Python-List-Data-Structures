9b74f4766412918a08257aaf0fc2e4ee323e691c
carol0221 <CarolEngo21@gmail.com>
2024-09-23 15:03:35 +0800

    festival_celebration_list.py

---
+++ festival_celebration_list.py
@@ -0,0 +1,15 @@
+festival_celebrations = ["Songkran", "Oktoberfest", "Rio Carnival", "New Orleans Mardi Gras", "Coachella", "Aloha Festivals", "Easter", "Holi Festival", "Obon", "Glastombury Festival"]
+
+print(festival_celebrations)
+
+
+print(festival_celebrations[4])
+
+festival_celebrations[2] = "Thanksgiving"
+print(festival_celebrations)
+
+del festival_celebrations[6]
+print(festival_celebrations)
+
+
+print(festival_celebrations[-1])
