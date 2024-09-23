6710c3eccb90e6ba03a224f38d9f22fcfdab85dd
carol0221 <CarolEngo21@gmail.com>
2024-09-23 13:22:37 +0800

    language_spoken_list.py

---
+++ language_spoken_list.py
@@ -0,0 +1,15 @@
+languages = ["English", "Tagalog", "Korean", "Chinese", "French", "Japanese", "Arabic", "Russian", "Portuguese", "Indonesian", "Hindi", "Vietnamese", "Marathi", "Urdu", "Bengali", "German", "Telugu", "Tamil", "Swahili" , "Punjabi"]
+
+print(languages)
+
+print(languages[12])
+
+languages[9] = "Spanish"
+print(languages)
+
+del languages[15]
+print(languages) 
+
+print(languages[5:11])
+
+print(languages[-1])
