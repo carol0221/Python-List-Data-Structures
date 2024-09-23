977fa01e6e52e47d8c6a7ab84fcc36449d6c6dc7
carol0221 <CarolEngo21@gmail.com>
2024-09-23 13:16:00 +0800

    planet_list.py

---
+++ planet_list.py
@@ -0,0 +1,13 @@
+planets = ["Mercury", "Venus", "Earth", "Saturn", "Jupiter", "Neptune", "Uranus", "Mars"]
+
+print(planets)
+
+print(planets[2])
+
+planets[6] = "Pluto"
+print(planets)
+
+del planets[3]
+print(planets)
+
+print(planets[-1])
