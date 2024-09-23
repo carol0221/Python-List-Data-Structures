df6d55dcc473ab0021c74e5c9befe986dbfb4901
carol0221 <CarolEngo21@gmail.com>
2024-09-23 13:07:22 +0800

    color_list.py

---
+++ color_list.py
@@ -0,0 +1,16 @@
+colors = ["Pink", "Yellow", "Green", "Blue", "Magenta", "Maroon", "Olive", "Orange", "Red", "Black", "White", "Gray", "Brown", "Gold", "Lavender"]
+
+print(colors)
+
+print(colors[9])
+
+colors[3] = "Purple"
+print(colors)
+
+del colors[4]
+print(colors)
+
+print(colors[1:8])
+
+print(colors[-1])
+
